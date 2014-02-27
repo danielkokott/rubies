82f6f2c6caaf96a027f1e51b2f548d22625970b8
Daniel Jozie Kokott <dako@berlingskemedia.dk>
2014-02-27 13:57:11 +0100

    parse af nordea CVS

--- app.js
+++ app.js
@@ -11,12 +11,52 @@ app.get('/', function(request, response) {
 
 app.get('/data/:id', function(request, response) {
 	if (request.params.id === 'poster') {
-		response.sendfile('./data/poster.csv');	
+		parseNordeaCvs('./data/poster.csv', function(data) {
+			response.send(data);
+		})
 	} else {
-		console.log(request.params["id"]);
-		response.sendfile('./data/' + request.params["id"] + '.json');	
+		response.sendfile('./data/' + request.params.id + '.json');	
 	}
 });
+
+function parseNordeaCvs(filename, callback) {
+	var fs = require('fs');
+	fs.readFile(filename, 'utf-8', function (err, data) {
+  		if (err) {
+    		throw err
+  		}
+  		var poster = {}
+  		var temp1 = data.split("\n");
+		for (var i = 0; i < temp1.length; i++) {
+			if (temp1[i] !== '' && temp1[i] !== '\r') {
+				var temp2 = temp1[i].split(";");
+				if (temp2[4] !== 'Saldo') {
+					var temp3 = temp2[0].split("-");
+					var year = temp3[2];
+					var month = temp3[1];
+					if (poster[year] === undefined) {
+						poster[year] = {};
+					}
+					if (poster[year][month] === undefined) {
+						poster[year][month] = [];
+					}
+
+ 					poster[year][month].push({
+		 				"date": temp3[1] + "-" + temp3[0] + "-" + temp3[2],
+		 				"text": temp2[1],
+		 				"amount": parseCustomNumber(temp2[3]),
+		 				"saldo": parseCustomNumber(temp2[4])
+			 		});
+ 				}
+	    	}
+	    }
+		callback({"start_saldo": 67516.19, "poster": poster});
+	})
+
+	function parseCustomNumber(number) {
+		return parseFloat(number.replace(',','.'));
+	}
+}
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,19 +1,7 @@
-//exports.load = function(file, callback) {
-//	console.log("loading")
-//	var fs = require('fs')
-//	fs.readFile(file, 'utf8', function (err, data) {
-//  		if (err) {
-//    		throw err
-//  		}
-//  		callback(data.split('\n'))
-//	})
-//}
-
 var rubiesApp = angular.module('rubiesApp', []);
  
 rubiesApp.controller('RubiesCtrl', function ($scope, $http) {
  	$http.get('data/poster', { headers: { "Content-Type": "text/plain; charset=utf-8" }}).success(function(data) {
-    	$scope.poster = data.split("\n");
-    	console.log($scope.poster);
+	 	$scope.poster = data.poster;
   	});
 });
